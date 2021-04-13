766d7333cece4a8042534398b2b57dc4a0a3fe5c
Falex030 <grynyshyn080@gmail.com>
2021-04-13 22:51:30 +0300

    Add new file

---
+++ config.py
@@ -0,0 +1,2 @@
+token = '5d99cb771b7fb20b41cc2cc82bb203e4'
+telegram_token = "1774997807:AAFywde8oUTyEZJmfB54Cw_6YBfN7-oJn2w"
---
+++ main.py
@@ -0,0 +1,65 @@
+import datetime
+from config import token
+from pyowm.utils import timestamps
+import requests
+from pprint import pprint
+
+
+def get_weather(city,token):
+
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+    try:
+        r = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}&units=metric')
+        data = r.json()
+        #pprint(data)
+
+        city = data['name']
+        temp = data['main']['temp']
+        feels_like = data['main']['feels_like']
+        temp_max = data ['main']['temp_max']
+        temp_min = data['main']['temp_min']
+        sunrise = datetime.datetime.fromtimestamp(data['sys']['sunrise']).strftime('%H:%M')
+        sunset = datetime.datetime.fromtimestamp(data['sys']['sunset']).strftime('%H:%M')
+        length_of_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+        wind = data['wind']['speed']
+        clouds = data['clouds']['all']
+
+        weather = data['weather'][0]['main']
+        if weather in code_to_smile:
+            w = code_to_smile[weather]
+        else:
+            w = " Я хз шо там, глянь сам в окно"
+
+
+
+        print(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f'Погода в городе {city}\n'
+              f'Температура {round(temp,1)}C° {w}\nОщущается на {round(feels_like,1)}C°\n'
+              f'Минимальная температура {round(temp_min,1)}C°, максимальная {round(temp_max,1)}C°\n'
+              f'Солнце встает в {sunrise}, и заходит в {sunset}, светловой день {length_of_day}\n'
+              f'Скорость ветра {round(wind,1)} км/ч\n'
+              f'Облачность {clouds} %\n'
+              f'Хорошего дня!'
+              )
+    except Exception as ex:
+        print(ex)
+        print('Проверте название города')
+
+def main():
+    city = input('Введите город:')
+    get_weather(city,token)
+
+
+if __name__ == '__main__':
+    main()
+
